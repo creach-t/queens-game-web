9ccb00222b33c3d2531188fc5cb100e2e8cc3d8b
CREACH-T <142243266+creach-t@users.noreply.github.com>
2025-06-25 10:00:35 +0200

    feat: create GameCell component with styling hooks

---
+++ src/components/GameCell.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { GameCell as GameCellType } from '../types/game';
+import './GameCell.css';
+
+interface GameCellProps {
+  cell: GameCellType;
+  size: number;
+  onClick: () => void;
+}
+
+export const GameCell: React.FC<GameCellProps> = ({ cell, size, onClick }) => {
+  const getCellContent = () => {
+    switch (cell.state) {
+      case 'queen':
+        return '♛';
+      case 'marker':
+        return '✗';
+      default:
+        return '';
+    }
+  };
+
+  const getCellClasses = () => {
+    let classes = 'game-cell';
+    
+    if (cell.isConflict) {
+      classes += ' game-cell--conflict';
+    }
+    
+    if (cell.isHighlighted) {
+      classes += ' game-cell--highlighted';
+    }
+    
+    if (cell.state === 'queen') {
+      classes += ' game-cell--queen';
+    } else if (cell.state === 'marker') {
+      classes += ' game-cell--marker';
+    }
+    
+    return classes;
+  };
+
+  return (
+    <button
+      className={getCellClasses()}
+      onClick={onClick}
+      style={{
+        backgroundColor: cell.regionColor,
+        width: size,
+        height: size,
+        minWidth: size,
+        minHeight: size
+      }}
+      title={`Cellule ${cell.row + 1}-${cell.col + 1} (Région ${cell.regionId})`}
+    >
+      <span className="game-cell__content">
+        {getCellContent()}
+      </span>
+    </button>
+  );
+};
